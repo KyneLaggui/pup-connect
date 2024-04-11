f49da1decb831a0a0e1883a6d45ae5a83af5dc65
Amado Niño Rei Punzalan <amadoninoreipunzalan@iskolarngbayan.pup.edu.ph>
2024-04-11 18:52:33 +0800

    fixed font weights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,34 @@
 import { Rubik } from "next/font/google";
 import "./globals.css";
 // import NavBar from "./customComp/NavBar";
-import localFont from "next/font/local"
+import localFont from "next/font/local";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
-const myFont = localFont({ src: '../fonts/InterDisplay-Black.ttf' })
+const myFont = localFont({
+  src: [
+    {
+      path: "../fonts/InterDisplay-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/InterDisplay-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/InterDisplay-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/InterDisplay-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -17,7 +40,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={myFont.className}>
         {/* <NavBar /> */}
-        {children}      
+        {children}
       </body>
     </html>
   );
