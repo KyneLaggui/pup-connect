1c163e7cf44c05b4c3975185cca907cc95e19999
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-04-13 21:37:42 +0800

    Make the index.js companies as 9

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -102,5 +102,132 @@ export const jobCardInfo = [
         "On-the-job training and skill development",
         "Opportunity to work with cutting-edge technology"
         ]
-    }
+    },
+    {
+        number: '5',
+        ratings: '3.5',
+        mode: 'Remote',
+        type: 'Internship',
+        reviews: '20',
+        salary: '₱15,000 per month', 
+        company: 'Microsoft Philippines Inc.',
+        title: 'Microsoft Student Program',
+        description: 'Dive into the world of tech with Microsoft internships! Gain hands-on experience on real projects across various fields, from coding to design. Explore programs designed for your experience level and learn from industry experts. Visit their Careers website to launch your journey!',
+        image: microsoftLogo,
+        tags: ["Advanced", "Explore", "Project-Based", "Diverse"],
+        location: "BGC, Philippines",
+        about: "At Microsoft Philippines Inc., we believe in nurturing talent and fostering innovation. Our Microsoft Student Program offers a gateway to the dynamic world of technology. Immerse yourself in a collaborative environment where your ideas are valued and your skills are honed. Join us on a journey of exploration and growth, where every challenge is an opportunity to excel.",
+        qualifications: [
+            "Proficiency in coding or design (depending on the specific internship)",
+            "Strong communication and teamwork skills",
+            "Enthusiasm for technology and innovation"
+        ],
+        benefits: [
+            "Hands-on experience on real projects",
+            "Learning opportunities from industry experts",
+            "Career development and growth prospects"
+        ]   
+    },
+    {
+        number: '6',
+        ratings: '4.2',
+        mode: 'On-site',
+        type: 'Full-Time',
+        reviews: '13',
+        salary: '₱35,000 per month', 
+        company: 'Oracle Philippines Inc.',
+        title: 'Network Administrator',
+        description: 'Our mission is to help people see data in new ways, discover insights, unlock endless possibilities.',
+        image: oracleLogo,
+        tags: ["Cloud", "Software", "Software", "Technology"],
+        location: "Quezon City, Philippines",
+        about: "Oracle Philippines Inc. is dedicated to revolutionizing the way data is perceived and utilized. As a Network Administrator, you'll be at the forefront of this transformation, ensuring seamless connectivity and optimal performance. Join our team of visionaries and redefine the boundaries of possibility.",
+        qualifications: [
+            "Bachelor's degree in Computer Science or related field",
+            "Proven experience in network administration",
+            "Strong analytical and problem-solving skills"
+        ],
+        benefits: [
+            "Competitive salary and benefits package",
+            "Opportunities for professional development and training",
+            "Exposure to cutting-edge technologies"
+        ]
+    },
+    {
+        number: '7',
+        ratings: '3.3',
+        mode: 'On-site',
+        reviews: '15',
+        type: 'Internship',
+        salary: '₱10,000 per month', 
+        company: 'Accenture Philippines Inc.',
+        title: 'Tech Support',
+        description: 'Become a vital part of our team as a Tech Support specialist, providing efficient solutions and exceptional customer service to resolve hardware and software issues swiftly.',
+        image: accentureLogo,
+        tags: ["Diverse", "Call Center", "Information Technology"],
+        location: "Cubao, Philippines",
+        about: "At Accenture Philippines Inc., we're committed to delivering exceptional service and driving technological advancement. As a Tech Support specialist, you'll play a pivotal role in providing innovative solutions and unparalleled customer support. Join us in shaping the future of technology and making a meaningful impact.",
+        qualifications: [
+            "Technical degree or equivalent experience",
+            "Prior experience in tech support or customer service",
+            "Knowledge of hardware and software troubleshooting"
+        ],
+        benefits: [
+            "Competitive salary and performance incentives",
+            "Comprehensive training and development programs",
+            "Opportunities for career advancement"
+        ]
+        
+    },
+    {
+        number: '8',
+        ratings: '3.3',
+        mode: 'On-site',
+        type: 'Part-Time',
+        reviews: '15',
+        salary: '₱15,000 per month', 
+        company: 'Accenture Philippines Inc.',
+        title: 'RJ 45 Maker',
+        description: "Join us as an RJ45 Maker where you'll assemble connectors onto Ethernet cables with precision and ensure seamless connectivity for our clients.",
+        image: accentureLogo,
+        tags: ["Engineer", "Networking", "RJ45", "Hardware"],
+        location: "Bulacan, Philippines",
+        about: "Join the innovative team at Accenture Philippines Inc. and become a crucial contributor to our network infrastructure. As an RJ45 Maker, you'll ensure seamless connectivity by meticulously assembling Ethernet cables. Embark on a rewarding journey where precision meets progress, and together, we'll build a better-connected world.",
+        qualifications: [
+            "Attention to detail and manual dexterity",
+            "Basic understanding of networking concepts",
+            "Previous experience in a similar role is a plus"
+        ],
+        benefits: [
+        "Competitive wages and benefits",
+        "On-the-job training and skill development",
+        "Opportunity to work with cutting-edge technology"
+        ]
+    },
+    {
+        number: '9',
+        ratings: '3.3',
+        mode: 'On-site',
+        type: 'Part-Time',
+        reviews: '15',
+        salary: '₱15,000 per month', 
+        company: 'Accenture Philippines Inc.',
+        title: 'RJ 45 Maker',
+        description: "Join us as an RJ45 Maker where you'll assemble connectors onto Ethernet cables with precision and ensure seamless connectivity for our clients.",
+        image: accentureLogo,
+        tags: ["Engineer", "Networking", "RJ45", "Hardware"],
+        location: "Bulacan, Philippines",
+        about: "Join the innovative team at Accenture Philippines Inc. and become a crucial contributor to our network infrastructure. As an RJ45 Maker, you'll ensure seamless connectivity by meticulously assembling Ethernet cables. Embark on a rewarding journey where precision meets progress, and together, we'll build a better-connected world.",
+        qualifications: [
+            "Attention to detail and manual dexterity",
+            "Basic understanding of networking concepts",
+            "Previous experience in a similar role is a plus"
+        ],
+        benefits: [
+        "Competitive wages and benefits",
+        "On-the-job training and skill development",
+        "Opportunity to work with cutting-edge technology"
+        ]
+    },
+    
 ]
