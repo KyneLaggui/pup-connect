8e9eeda238444c0a9c0562da55b33e0f1d1f6407
Amado Niño Rei Punzalan <amadoninoreipunzalan@iskolarngbayan.pup.edu.ph>
2024-04-13 16:02:34 +0800

    fixed bg

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,9 +1,6 @@
-import accentureLogo from '../assets/accenture.png'
-import microsoftLogo from '../assets/microsoft.jpg'
-import oracleLogo from '../assets/oracle.png'
+import accentureLogo from "../assets/accenture.png";
+import microsoftLogo from "../assets/microsoft.jpg";
+import oracleLogo from "../assets/oracle.png";
+import heroBG from "../assets/bg/hero-page-background.png";
 
-export {
-    accentureLogo,
-    microsoftLogo,
-    oracleLogo
-}
+export { accentureLogo, microsoftLogo, oracleLogo, heroBG };
