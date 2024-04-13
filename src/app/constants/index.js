f230f2d145a3616fe4be54002f31e4bc497a8f7b
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-04-13 13:58:20 +0800

    Changed the tags into each cards

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,48 +1,34 @@
 import { microsoftLogo, oracleLogo, accentureLogo } from '@assets/index';
 
 
-export const filterMicrosoft = [
-    {
-        name: "Advanced",
-        tags: '1'
-    },
-    {
-        name: "Explore",
-        tags: '2'
-    },
-    {
-        name: "Project-Based",
-        tags: '3'
-    },
-    {
-        name: "Diverse",
-        tags: '4'
-    }
-];
-
 export const jobCardInfo = [
     {
         number: '1',
         title: 'Microsoft Student Program',
         description: 'Dive into the world of tech with Microsoft internships! Gain hands-on experience on real projects across various fields, from coding to design. Explore programs designed for your experience level and learn from industry experts. Visit their Careers website to launch your journey!',
         image: microsoftLogo,
+        tags: ["Advanced", "Explore", "Project-Based", "Diverse"],
+        
     },
     {
         number: '2',
         title: 'Oracle Philippines',
         description: 'Our mission is to help people see data in new ways, discover insights, unlock endless possibilities.',
         image: oracleLogo,
+        tags: ["Cloud", "Software", "Software", "Technology"],
     },
     {
         number: '3',
         title: 'Tech Support',
         description: 'Become a vital part of our team as a Tech Support specialist, providing efficient solutions and exceptional customer service to resolve hardware and software issues swiftly.',
         image: accentureLogo,
+        tags: ["Diverse", "Call Center", "Information Technology"],
     },
     {
         number: '4',
         title: 'RJ 45 Maker',
         description: "Join us as an RJ45 Maker where you'll assemble connectors onto Ethernet cables with precision and ensure seamless connectivity for our clients.",
         image: accentureLogo,
+        tags: ["Engineer", "Networking", "RJ45", "Hardware"],
     }
 ]
