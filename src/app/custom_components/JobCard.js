18aebc9b6544935e1d5f42c8b0e4aa4f1a06479b
Amado Niño Rei Punzalan <amadoninoreipunzalan@iskolarngbayan.pup.edu.ph>
2024-04-15 00:39:56 +0800

    added hover effect on job card

--- src/app/custom_components/JobCard.js
+++ src/app/custom_components/JobCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+
 import {
   Card,
   CardContent,
@@ -9,10 +10,16 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 import { Tag } from "@/app/custom_components/Tag";
+import { useEffect } from "react";
 
 const JobCard = ({ title, description, image, tags }) => {
+  // useEffect(() => {
+  //   const copy = document.querySelector(".slide").cloneNode(true);
+  //   document.querySelector(".slide-container").appendChild(copy);
+  // }, []);
+
   return (
-    <Card className="max-w-[296px] h-[160px] min-w-[240px] flex justify-between items-center flex-col p-5 border border-blue-100 bg-background">
+    <Card className="slide-parent max-w-[296px] h-[160px] min-w-[240px] flex justify-between items-center flex-col p-5 border border-blue-100 bg-background">
       <div className="flex flex-col justify-start items-center gap-3">
         <div className="w-full flex items-center justify-start gap-2 flex-row">
           <Image
@@ -31,12 +38,21 @@ const JobCard = ({ title, description, image, tags }) => {
         </CardDescription>
       </div>
 
-      <div className="mt-3 w-full overflow-hidden flex gap-1">
-        {tags.map((tag, index) => (
-          <Tag key={index} size="sm">
-            {tag}
-          </Tag>
-        ))}
+      <div className="mt-3 w-full overflow-hidden flex gap-1 slide-container">
+        <div className="slide flex gap-1">
+          {tags.map((tag, index) => (
+            <Tag key={index} size="sm">
+              {tag}
+            </Tag>
+          ))}
+        </div>
+        <div className="slide flex gap-1">
+          {tags.map((tag, index) => (
+            <Tag key={index} size="sm">
+              {tag}
+            </Tag>
+          ))}
+        </div>
       </div>
     </Card>
   );
