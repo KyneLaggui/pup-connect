377d10126c91389534be8817a244be08387f393a
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2024-05-08 21:11:55 +0800

    Synching progress.

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
+     * Feel free to modify this pattern to include more paths..
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
