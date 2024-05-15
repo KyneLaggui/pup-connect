b3b93e9e7d967ad86dcd0878757b74da06f19321
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-05-15 15:17:33 +0800

    Add: Needed Stepper Components

--- src/app/custom_components/FormUser.js
+++ src/app/custom_components/FormUser.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import StepperControl from './StepperControl'
+import StepperForm from './StepperForm'
+
 
 const FormUser = () => {
   return (
-    <div>
-      
+    <div className='md: w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white'>
+        <div className='container horizontal mt-5'>
+            <StepperForm />
+        </div>
+        
+        <StepperControl />
     </div>
   )
 }
