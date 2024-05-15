2098e9f8dc68d844e19b06adf69d77e3fca60885
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-05-15 15:27:17 +0800

    Add: Buttons in StepperControl

--- src/app/custom_components/StepperControl.jsx
+++ src/app/custom_components/StepperControl.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const StepperControl = () => {
   return (
-    <div>
-      
+    <div className='container flex justify-around mt-4 mb-8'>
+      <Button variant="secondary" size="lg">Back</Button>
+      <Button size="lg">Next</Button>
     </div>
   )
 }
