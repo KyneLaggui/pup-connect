5170891eb4af36ff11618b4ef3bdb7b4afb9a375
Amado Niño Rei Punzalan <amadoninoreipunzalan@iskolarngbayan.pup.edu.ph>
2024-05-15 20:08:38 +0800

    Add action columns for data table
    - Add action columns for data table.
    - Initialize column names.
    - Change data table header color to a darker color, to have a more contrast.

--- src/app/pages/moa-status/columns.tsx
+++ src/app/pages/moa-status/columns.tsx
@@ -2,26 +2,71 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
+export type User = {
   id: string;
   amount: number;
   status: "pending" | "processing" | "success" | "failed";
   email: string;
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "status",
     header: "Status",
+  },
+  {
+    accessorKey: "company",
+    header: "Company",
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "date",
+    header: "Date",
   },
+  {
+    accessorKey: "action",
+    header: "Actions",
+    cell: ({ row }) => {
+      const user = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.id)}
+            >
+              Copy user ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View user details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  }
 ];
--- src/app/pages/moa-status/page.tsx
+++ src/app/pages/moa-status/page.tsx
@@ -1,30 +1,35 @@
 // Components
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 // Custom component
 import Sidebar from "@/app/custom_components/Sidebar";
 
 // Data Table
-import { Payment, columns } from "./columns";
+import { User, columns } from "./columns";
 import { DataTable } from "./data-table";
 
 // Wrapper
 import ApplicantOnlyPage from "@/app/layouts/ApplicantOnlyPage"
 
-async function getData(): Promise<Payment[]> {
+async function getData(): Promise<User[]> {
   // Fetch data from your API here.
   return [
     {
-      id: "728ed52f",
-      amount: 100,
+      id: "a1b2c3d4",
+      amount: 59.99,
       status: "pending",
-      email: "m@example.com",
+      email: "johndoe@mail.com",
     },
     {
       id: "728ed52f",
       amount: 100,
       status: "pending",
-      email: "m@example.com",
+      email: "peterparker@mail.com",
     },
     // ...
   ];
@@ -38,8 +43,9 @@ export default async function DemoPage() {
     <>
       <div className="flex">
         <div className="w-full flex justify-center container-sidebar">
-          okay
-          {/* <DataTable columns={columns} data={data} /> */}
+          <h1 className="text-2xl font-semibold tracking-tight">MOA Status</h1>
+          <p className="text-md font-medium text-muted-foreground mb-4">This is the list of MOA that has been processed and being processed.</p>
+          <DataTable columns={columns} data={data} />
         </div>
       </div>
     </>
