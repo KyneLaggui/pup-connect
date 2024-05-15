0433fb61c11e04d4a4ddb35b7dfa7be863c8b1f0
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-05-15 15:06:54 +0800

    Update: Made it as component remove it as page

--- src/app/custom_components/FormUser.js
+++ src/app/custom_components/FormUser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const FormUser = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default FormUser
