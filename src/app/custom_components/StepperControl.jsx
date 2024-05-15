45126cf2fa89d2254cf44ef45cf53f1efb3b237a
Kyne Domerei N. Laggui <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2024-05-15 20:39:47 +0800

    Fix: The next and confirm button, Add: Final Text

--- src/app/custom_components/StepperControl.jsx
+++ src/app/custom_components/StepperControl.jsx
@@ -5,7 +5,7 @@ const StepperControl = ({ handleClick, currentStep, steps }) => {
   return (
     <div className='container flex justify-around mt-4 mb-8'>
       <Button variant="secondary" size="lg" onClick={()=>handleClick("")} className={`${currentStep === 1 ? "opacity-50 cursor-not-allowed" : ""}`} >Back</Button>
-      <Button size="lg" onClick={()=>handleClick("next")}>{currentStep === steps.length -1 ? "Confirm" : "Next"}</Button>
+      <Button size="lg" onClick={()=>handleClick("next")}>{currentStep === steps.length - 0 ? "Confirm" : "Next"}</Button>
     </div>
   )
 }
--- src/app/custom_components/userSteps/Final.js
+++ src/app/custom_components/userSteps/Final.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Final = () => {
   return (
     <div>
-      
+      <h1>Complete You have Created your Account</h1>
     </div>
   )
 }
